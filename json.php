ada46412c5e243b66e444bf5640c68db9f41cafa
Ahmedie-m <61509242+Ahmedie-m@users.noreply.github.com>
2020-05-17 19:26:03 +0300

    Added sorting from descending order to show last 50 results
    Added sorting from descending order to show last 50 results

--- json.php
+++ json.php
@@ -1,6 +1,6 @@
 <?php
-$db = new SQLite3('/mnt/sda1/sensor.db');
-$results = $db->query('SELECT id,(julianday(time)- 2440587.5)*86400000 AS time,type,red,green,blue FROM sensor_data ORDER BY id LIMIT 50');
+$db = new SQLite3('mnt/sda1/sensor.db');
+$results = $db->query('SELECT id,(julianday(time)- 2440587.5)*86400000 AS time,type,red,green,blue FROM sensor_data ORDER BY id DESC LIMIT 50');
 
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     $jsonArray[] = $row;
